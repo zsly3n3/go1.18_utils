dbed4f7e79c678514a7ecca12fdc937ebb80d210
cxp <cxp@pc.com>
2022-08-27 07:18:37 +0800

    add

--- generate/generate.go
+++ generate/generate.go
@@ -27,3 +27,25 @@ func ClusterUniqueID(pid int) (string, error) {
 	rs := common.Md5(fmt.Sprintf(`%d%d`, id, pid))
 	return rs, nil
 }
+
+//生成用户guid
+//ts为用户创建时的时间戳,占guid的高33位
+//order为用户顺序的数字id,占guid的中27位
+//region为用户创建时的区域,占guid的低4位
+func CreateUserGuid(ts, order, region uint64) uint64 {
+	tmp0 := ts << 31
+	tmp1 := order << 4
+	guid := region | tmp1 | tmp0
+	return guid
+}
+
+//根据用户guid解析出时间戳,顺序id,区域
+func GetDataFromGuid(guid uint64) (uint64, uint64, uint64) {
+	ts := guid >> 31
+	var tmp2 uint64 = 0x7ffffff //27位都是1的16进制
+	tmp3 := guid >> 4
+	id := tmp3 & tmp2
+	var tmp4 uint64 = 0xf //4位都是1的16进制
+	region := guid & tmp4
+	return ts, id, region
+}
--- main.go
+++ main.go
@@ -2,9 +2,8 @@ package main
 
 import (
 	"fmt"
-	"github.com/zsly3n3/go1.18_utils/syncAdditional"
+	"github.com/zsly3n3/go1.18_utils/generate"
 	"golang.org/x/exp/constraints"
-	"sync"
 )
 
 func main() {
@@ -15,15 +14,47 @@ func main() {
 	//num := len(s1)
 	//sortBy.MergeSortNumeric(false, s1, 0, num-1)
 	//Test(s1)
+	//
+	//smap := sync.Map{}
+	//smap.Store(`t1`, 1)
+	//count := syncAdditional.LenSyncMap(&smap)
+	//fmt.Println(count)
+	Abc()
 
-	smap := sync.Map{}
-	smap.Store(`t1`, 1)
-	count := syncAdditional.LenSyncMap(&smap)
-	fmt.Println(count)
 }
 
 func Test[T constraints.Ordered](arr []T) {
 	for _, v := range arr {
 		fmt.Println(v)
 	}
+}
+
+func Abc() {
+	var ts uint64 = 1861545531 //高33位
+	var region uint64 = 3      //低4位
+	n := 100000
+	mp := make(map[uint64]struct{})
+	for i := 1; i <= n; i++ {
+		id := uint64(i)
+		guid := generate.CreateUserGuid(ts, id, region)
+		_, ok := mp[guid]
+		if ok {
+			fmt.Println(`isExist`)
+			break
+		} else {
+			mp[guid] = struct{}{}
+		}
+	}
+	for k := range mp {
+		ts0, _, region0 := generate.GetDataFromGuid(k)
+		if ts0 != ts {
+			fmt.Println(`ts error`)
+			break
+		}
+		if region0 != region {
+			fmt.Println(`region error`)
+			break
+		}
+	}
+	fmt.Println(`---over`)
 }
