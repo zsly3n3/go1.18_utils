343be8b736655ac251f400ac3d45c3616264b857
cxp <cxp@pc.com>
2022-10-12 09:34:26 +0800

    add

--- generate/generate.go
+++ generate/generate.go
@@ -10,15 +10,14 @@ import (
 var sf = sonyflake.NewSonyflake(sonyflake.Settings{
 	StartTime: time.Date(2020, 12, 16, 0, 0, 0, 0, time.Local)})
 
-//非分布式唯一
-//func Unique() (string, error) {
-//	id, err := sf.NextID()
-//	sid := fmt.Sprint(id)
-//	return sid, err
-//}
+// 非分布式唯一
+func NextID() (uint64, error) {
+	id, err := sf.NextID()
+	return id, err
+}
 
-//分布式唯一ID:md5(雪花ID和进程ID)
-//传入进程ID
+// 分布式唯一ID:md5(雪花ID和进程ID)
+// 传入进程ID
 func ClusterUniqueID(pid int) (string, error) {
 	id, err := sf.NextID()
 	if err != nil {
@@ -28,10 +27,10 @@ func ClusterUniqueID(pid int) (string, error) {
 	return rs, nil
 }
 
-//生成用户guid
-//ts为用户创建时的时间戳,占guid的高33位
-//order为用户顺序的数字id,占guid的中27位
-//region为用户创建时的区域,占guid的低4位
+// 生成用户guid
+// ts为用户创建时的时间戳,占guid的高33位
+// order为用户顺序的数字id,占guid的中27位
+// region为用户创建时的区域,占guid的低4位
 func CreateUserGuid(ts, order, region uint64) uint64 {
 	tmp0 := ts << 31
 	tmp1 := order << 4
@@ -39,7 +38,7 @@ func CreateUserGuid(ts, order, region uint64) uint64 {
 	return guid
 }
 
-//根据用户guid解析出时间戳,顺序id,区域
+// 根据用户guid解析出时间戳,顺序id,区域
 func GetDataFromGuid(guid uint64) (uint64, uint64, uint64) {
 	ts := guid >> 31
 	var tmp2 uint64 = 0x7ffffff //27位都是1的16进制
